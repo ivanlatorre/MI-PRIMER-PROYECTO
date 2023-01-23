09d35527ecda0f5ec6c4f6fa84765302b62d3abc
analiaveronicavalero <123344496+analiaveronicavalero@users.noreply.github.com>
2023-01-23 13:17:50 -0300

    Add files via upload

---
+++ MI PRIMER PROYECTO/Mi-CV.js
@@ -0,0 +1,6 @@
+document.getElementById("boton" ).onclick= function (){     // este ejemplo es para hacer funcionar el boton click
+    console.log(" capturamos el evento click");
+    document.getElementById("demo" ).innerHTML= " Estamos probando nuestro primer evento en JS";}
+
+
+
