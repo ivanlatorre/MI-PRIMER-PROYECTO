5868c0c7e029ace4c6e63264b1afed7bf1fabe92
analiaveronicavalero <123344496+analiaveronicavalero@users.noreply.github.com>
2023-01-30 23:34:25 -0300

    Delete Mi-CV.js

--- MI PRIMER PROYECTO/Mi-CV.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("boton" ).onclick= function (){     // este ejemplo es para hacer funcionar el boton click
-    console.log(" capturamos el evento click");
-    document.getElementById("demo" ).innerHTML= " Estamos probando nuestro primer evento en JS";}
-
-
-
